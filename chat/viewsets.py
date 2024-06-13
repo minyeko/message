6d2b6904f469b2187e02caa4e9e8dcd22c26eeb4
minyeko <minyeko@outlook.com>
2024-06-13 12:49:40 +1200

    whatever

--- chat/viewsets.py
+++ chat/viewsets.py
@@ -1,12 +1,12 @@
-
+from chat.models import ChatRoom, Message
+from chat.serializers import ChatRoomSerializer,MessageSerializer
 from rest_framework import viewsets
-from .models import ChatRoom, Message
-from .serializers import ChatRoomSerializer, MessageSerializer
+
 
 class ChatRoomViewSet(viewsets.ModelViewSet):
     queryset = ChatRoom.objects.all()
     serializer_class = ChatRoomSerializer
- 
+
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
